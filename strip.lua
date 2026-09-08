4908f9d2c3e67e6bdc268e018d9f064b3248c253
souny-mlv <souny@kokkoksole.com>
2026-09-08 15:38:17 +0700

    mmm

--- strip.lua
+++ strip.lua
@@ -2,11 +2,13 @@ function from_log_path(tag, timestamp, record)
     local path = record["filepath"] or record["path"] or ""
     -- /etc/docker/logs/81f96ffa78df.log → 81f96ffa78df
     local name = path:match("([^/]+)%.log$")
-    if type(name) == "string" and name ~= "" then
-        record["container_name"] = name:lower()
-        record["container_id"] = name:lower()
+    if type(name) ~= "string" or name == "" then
+        name = "unknown"
     else
-        record["container_name"] = "unknown"
+        name = name:lower()
     end
+
+    record["container_name"] = name
+    record["container_id"] = name
     return 1, timestamp, record
 end
